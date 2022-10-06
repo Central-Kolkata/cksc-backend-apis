de53169be4342fafc56b1e746512b07d4e330af4
suvishakha <85212955+suvishakha@users.noreply.github.com>
2022-10-06 23:43:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,6 +15,19 @@ app.use(`/api/atom`, require("./routes/atom-routes"));
 
 app.use(errorHandler);
 
+app.use((req, res, next) =>
+{
+	res.setHeader("Access-Control-Allow-Origin", "*");
+	res.setHeader(
+		"Access-Control-Allow-Methods",
+		"GET, POST, OPTIONS, PUT, PATCH, DELETE",
+	);
+	res.setHeader(
+		"Access-Control-Allow-Headers",
+		"Content-Type, Authorization, Accept-Language",
+	);
+});
+
 app.listen(port, () =>
 {
 	console.log(`Server started on port ${port}`);
