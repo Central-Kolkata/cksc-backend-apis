055c18419c880652a1cb23d1e98216a8866002af
mihir-suvishakha <119147059+mihir-suvishakha@users.noreply.github.com>
2022-12-15 21:27:50 +0530

    user management changes for admin panel (#1)

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv").config();
 const { errorHandler } = require("./middlewares/error-middleware");
 const connectMongoDB = require("./config/db");
+const cors = require('cors');
 
 connectMongoDB();
 const port = process.env.PORT || 5001;
@@ -22,6 +23,7 @@ app.use((req, res, next) =>
 	next();
 });
 
+app.use(cors())
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: false }));
 
