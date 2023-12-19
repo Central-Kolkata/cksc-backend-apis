320d12f1a36c9250cbdaed2d65127725d4593621
suvishakha <85212955+suvishakha@users.noreply.github.com>
2023-12-19 17:08:42 +0530

    made this workable. Hardcodings need to be removed

--- routes/icici-routes.js
+++ routes/icici-routes.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { fetchPaymentRequestURL, receivePaymentResponse, verifyTransaction } = require("../controllers/icici-controller");
+const { fetchOneTimePaymentRequestURL, receiveOneTimePaymentResponse, fetchPaymentRequestURL, receivePaymentResponse, verifyTransaction } = require("../controllers/icici-controller");
 
 router.route(`/fetchPaymentRequestURL`).post(fetchPaymentRequestURL);
 router.route(`/receivePaymentResponse`).post(receivePaymentResponse);
+router.route(`/fetchOneTimePaymentRequestURL`).post(fetchOneTimePaymentRequestURL);
+router.route(`/receiveOneTimePaymentResponse`).post(receiveOneTimePaymentResponse);
 router.route(`/verifyTransaction`).post(verifyTransaction);
 
 module.exports = router;
